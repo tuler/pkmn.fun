c4285b1aee0400d1e8b66b86804e58444b437b11
Danilo Tuler <tuler@pobox.com>
2025-02-12 23:21:28 -0500

    Wallet primary color

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { MantineProvider } from "@mantine/core";
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { lightTheme, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { type ReactNode, useState } from "react";
 import { WagmiProvider } from "wagmi";
@@ -12,12 +12,19 @@ import theme from "@/theme";
 export function Providers(props: { children: ReactNode }) {
     const [config] = useState(() => getConfig());
     const [queryClient] = useState(() => new QueryClient());
+    const walletTheme = lightTheme({
+        accentColor: "#ea7149",
+        borderRadius: "small",
+    });
 
     return (
         <MantineProvider theme={theme}>
             <WagmiProvider config={config}>
                 <QueryClientProvider client={queryClient}>
-                    <RainbowKitProvider showRecentTransactions={true}>
+                    <RainbowKitProvider
+                        showRecentTransactions={true}
+                        theme={walletTheme}
+                    >
                         {props.children}
                     </RainbowKitProvider>
                 </QueryClientProvider>
