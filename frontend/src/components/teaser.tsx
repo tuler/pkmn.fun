8654944db3906b59897c4951df93410d463df890
Danilo Tuler <tuler@pobox.com>
2025-02-14 09:29:37 -0500

    Improve teaser on mobile

--- frontend/src/components/teaser.tsx
+++ frontend/src/components/teaser.tsx
@@ -1,10 +1,11 @@
 import { TeamSpecies } from "@/components/team/species";
-import { ActionIcon, Anchor, Button, Group, Stack } from "@mantine/core";
+import { ActionIcon, Anchor, Button, Stack, useMatches } from "@mantine/core";
 import { PokemonSet } from "@pkmn/sets";
 import { TeamGenerators } from "@pkmn/randoms";
 import { IconBrandX, IconMoodCrazyHappy } from "@tabler/icons-react";
 import { FC, useState } from "react";
 import { Dex, Format, PRNG, PRNGSeed } from "@pkmn/sim";
+import { useViewportSize } from "@mantine/hooks";
 
 const DEFAULT_FORMAT = Dex.formats.get("gen9randombattle");
 
@@ -21,24 +22,35 @@ export const Teaser: FC = () => {
         randomTeam(DEFAULT_FORMAT, PRNG.get(`sodium,0`)),
     );
 
+    const { height } = useViewportSize();
+    const count = useMatches({
+        base: 1,
+        md: 6,
+    });
+    const gap = useMatches({
+        base: 0,
+        md: 50,
+    });
+
     return (
-        <Stack gap={50} p={100}>
-            <Group justify="center">
-                <Anchor href="https://x.com/pkmn_fun" target="_blank">
-                    <ActionIcon
-                        size="lg"
-                        color="gray"
-                        variant="subtle"
-                        radius="lg"
-                    >
-                        <IconBrandX
-                            style={{ width: "70%", height: "70%" }}
-                            stroke={1.5}
-                        />
-                    </ActionIcon>
-                </Anchor>
-            </Group>
-            <Group justify="center">
+        <Stack
+            gap={50}
+            pt={100}
+            pb={200}
+            align="center"
+            justify="space-around"
+            h={height}
+        >
+            <Anchor href="https://x.com/pkmn_fun" target="_blank">
+                <ActionIcon size="lg" color="gray" variant="subtle" radius="lg">
+                    <IconBrandX
+                        style={{ width: "70%", height: "70%" }}
+                        stroke={1.5}
+                    />
+                </ActionIcon>
+            </Anchor>
+            <Stack gap={gap} mih={400} justify="flex-end" align="center">
+                <TeamSpecies team={team?.slice(0, count)} />
                 <Button
                     leftSection={<IconMoodCrazyHappy />}
                     variant="gradient"
@@ -47,8 +59,7 @@ export const Teaser: FC = () => {
                 >
                     I'm feeling lucky!
                 </Button>
-            </Group>
-            <TeamSpecies team={team} />
+            </Stack>
         </Stack>
     );
 };
