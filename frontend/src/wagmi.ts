6895a106aac37576c64a25c9ca3d6620e1194b30
Danilo Tuler <tuler@pobox.com>
2025-02-14 18:08:35 -0500

    Testnet deployment

--- frontend/src/wagmi.ts
+++ frontend/src/wagmi.ts
@@ -5,7 +5,10 @@ export function getConfig() {
     return getDefaultConfig({
         appName: "pkmn.fun",
         projectId: process.env.NEXT_PUBLIC_REOWN_PROJECT_ID!,
-        chains: [anvil, holesky],
+        chains:
+            process.env.NODE_ENV === "development"
+                ? [anvil, holesky]
+                : [holesky],
         ssr: true,
     });
 }
