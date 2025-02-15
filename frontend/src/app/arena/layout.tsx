5dfd31e4c0cb0fc8af701d8242205305acff2727
Danilo Tuler <tuler@pobox.com>
2025-02-14 23:51:03 -0500

    Arena to home link

--- frontend/src/app/arena/layout.tsx
+++ frontend/src/app/arena/layout.tsx
@@ -1,11 +1,16 @@
-import { Stack } from "@mantine/core";
-import { type ReactNode } from "react";
 import { Header } from "@/components/header";
+import { Anchor, Breadcrumbs, Stack, Text } from "@mantine/core";
+import { type ReactNode } from "react";
 
 export default async function Layout(props: { children: ReactNode }) {
     return (
         <Stack p={10}>
-            <Header />
+            <Header>
+                <Breadcrumbs>
+                    <Anchor href="/">Home</Anchor>
+                    <Text>Arena</Text>
+                </Breadcrumbs>
+            </Header>
             {props.children}
         </Stack>
     );
