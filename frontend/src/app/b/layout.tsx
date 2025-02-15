362b4b804e33b722ffdeaa720cc332dee5fba24c
Danilo Tuler <tuler@pobox.com>
2025-02-15 01:12:55 -0500

    Breadcrumbs in battle view

--- frontend/src/app/b/layout.tsx
+++ frontend/src/app/b/layout.tsx
@@ -1,11 +1,16 @@
-import { Stack } from "@mantine/core";
-import { type ReactNode } from "react";
 import { Header } from "@/components/header";
+import { Anchor, Breadcrumbs, Stack } from "@mantine/core";
+import { type ReactNode } from "react";
 
 export default async function Layout(props: { children: ReactNode }) {
     return (
         <Stack p={10}>
-            <Header />
+            <Header>
+                <Breadcrumbs>
+                    <Anchor href="/">Home</Anchor>
+                    <Anchor href="/arena">Arena</Anchor>
+                </Breadcrumbs>
+            </Header>
             {props.children}
         </Stack>
     );
