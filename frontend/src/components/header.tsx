8b57b862dbcff8a15f421470f1bc7445ecea9fe8
Danilo Tuler <tuler@pobox.com>
2025-02-14 23:50:44 -0500

    Header with children

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,11 +1,15 @@
-import { Group } from "@mantine/core";
+import { Group, GroupProps } from "@mantine/core";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { FC } from "react";
+import { FC, ReactNode } from "react";
 
-export const Header: FC = () => {
+export interface HeaderProps extends GroupProps {
+    children?: ReactNode;
+}
+
+export const Header: FC<HeaderProps> = ({ children, ...otherProps }) => {
     return (
-        <Group justify="space-between" mih={50}>
-            <Group gap={5}></Group>
+        <Group justify="space-between" mih={50} {...otherProps}>
+            <Group gap={5}>{children}</Group>
             <Group gap={5}>
                 <ConnectButton />
             </Group>
